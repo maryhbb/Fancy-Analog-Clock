82696192e1f4a2e300da1c1e638fc8ee6bb4ade7
Mary <mary_hbb@yahoo.com>
2023-05-13 19:54:22 +0200

    Listing TODO in Scripte

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ *  TODO for creating 60 ticks
+        1. create line element
+        2. append to svg as child
+        3. add attributes on line element
+
+        2. find point for each tick 
+        3. draw ticks
+ */
+
