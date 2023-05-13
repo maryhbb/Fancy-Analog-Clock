09b0d5fe5fea65f052ac9a40b3a344ab2da56872
Mary <mary_hbb@yahoo.com>
2023-05-13 20:02:49 +0200

    Update TODO in Script

--- script.js
+++ script.js
@@ -4,8 +4,10 @@
         2. append to svg as child
         3. add attributes on line element
 
-        2. find point for each tick 
-        3. draw ticks
-        
+        4. find point for each tick 
+            function to convert degree to radian
+            declare fix offset
+        5. draw ticks
+
  */
 
