a6abb582caf8f7f4111218cb1a230635851f62f2
Mary <mary_hbb@yahoo.com>
2023-05-13 20:19:00 +0200

    Implement createLine function

--- script.js
+++ script.js
@@ -11,3 +11,15 @@
 
  */
 
+//create line element
+function createLine(p1, p2, strong) {
+  const svgEl = document.querySelector("svg"); // get svg Element
+  const lineEl = document.createElementNS("http://www.w3.org/2000/svg", line); //create line Element
+  svgEl.appendChild("lineEl"); // append line Element into SVG as child
+  lineEl.setAttribute("x1", p1.x); // set Attributes on line Element
+  lineEl.setAttribute("y1", p1.y);
+  lineEl.setAttribute("x2", p2.x);
+  lineEl.setAttribute("y1", p2.y);
+  lineEl.setAttribute("stroke", "black");
+  lineEl.setAttribute("stroke-width", strong ? 2.5 : 1);
+}
